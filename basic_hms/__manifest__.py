a9fd2f08a358ea58c2f0aaaeb970ab5316592335
Jacob Sanchez <info@netburo.es>
2024-04-28 18:59:21 +0200

    Update __manifest__.py

--- basic_hms/__manifest__.py
+++ basic_hms/__manifest__.py
@@ -3,7 +3,8 @@
 
 {
 
-    "name": "Basic Hospital Management System in Odoo",
+    #"name": "Basic Hospital Management System in Odoo",
+    "name": "NovooClinic",
     "version": "17.0.0.1",
     "currency": 'EUR',
     "summary": "Apps basic Hospital Management system Healthcare Management Clinic Management apps manage clinic manage Patient hospital manage Healthcare system Patient Management Hospital Management Healthcare Management Clinic Management hospital Lab Test Request",
